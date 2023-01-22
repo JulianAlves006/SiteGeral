088d80df115e51eecd53e79f0203d20a42d9c63e
JulianAlves006 <julianalvesinstinto@gmail.com>
2023-01-22 00:59:18 -0300

    mudanças totais

---
+++ main.js
@@ -0,0 +1,33 @@
+document.addEventListener('click', e =>{
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if(tag === 'a'){
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el){
+    try{
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+        if(response.status !== 200) throw new Error('ERRO 404 PAGE NOT FOUND');
+        const html = await response.text();
+        carregaResultado(html);
+    }
+    catch(e){
+        console.error(e);
+    }
+}
+
+function carregaResultado(response){
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = response;
+}
+
+fetch('pagina4.html').then(resposta => {
+    if(resposta.status !== 200) throw new Error('ERRO 404 NOSSO');
+    return resposta.text;
+}).then(html => console.log(html))
+.catch(e => console.error(e));
