59361206dd653dcf254ee8e0c4247ebdde3510df
JulianAlves006 <julianalvesinstinto@gmail.com>
2023-01-23 00:20:21 -0300

    arrumando bugs

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.addEventListener('click', e =>{
     const el = e.target;
     const tag = el.tagName.toLowerCase();
-
-    if(tag === 'a'){
+    console.log(el);
+    if(tag === 'button'){
         e.preventDefault();
         carregaPagina(el);
     }
@@ -25,9 +25,3 @@ function carregaResultado(response){
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = response;
 }
-
-fetch('pagina4.html').then(resposta => {
-    if(resposta.status !== 200) throw new Error('ERRO 404 NOSSO');
-    return resposta.text;
-}).then(html => console.log(html))
-.catch(e => console.error(e));
